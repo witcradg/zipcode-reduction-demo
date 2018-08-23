4098ee5fca588400acd19d4ec017d8e9888e46f9
Dean Witcraft <witcradg@gmail.com>
2018-08-23 15:53:53 -0700

    Setup Maven project archetype

---
+++ zipcodereduction/src/main/java/us/witcraft/zipcodereduction/App.java
@@ -0,0 +1,13 @@
+package us.witcraft.zipcodereduction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
